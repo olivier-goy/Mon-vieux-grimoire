3619279f2b4f7642ae252b8db96e50e1c03d1227
olivier-goy <olivier.goy.37@gmail.com>
2023-06-16 12:38:47 +0200

    deleted code test

--- app.js
+++ app.js
@@ -25,23 +25,4 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use((req, res, next) => {
-  console.log("Requête reçue !");
-  next();
-});
-
-app.use((req, res, next) => {
-  res.status(201);
-  next();
-});
-
-app.use((req, res, next) => {
-  res.json({ message: "Votre requête a bien été reçue !" });
-  next();
-});
-
-app.use((req, res, next) => {
-  console.log("Réponse envoyée avec succès !");
-});
-
 module.exports = app;
