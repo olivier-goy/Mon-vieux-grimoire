a3eb142aa2da666c56297c8e9fa64c0811e3c1c4
olivier-goy <olivier.goy.37@gmail.com>
2023-06-16 13:32:44 +0200

    create file book inner controllers folder and create function job for get book all

---
+++ controllers/book.js
@@ -0,0 +1,7 @@
+const Book = require("../models/Book");
+
+exports.getAllBooks = (req, res, next) => {
+  Book.find()
+    .then((books) => res.status(200).json(books))
+    .catch((error) => res.status(400).json(error));
+};
