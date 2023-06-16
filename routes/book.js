22ce867ff3f301747803f0b268d4ae84b4df57c0
olivier-goy <olivier.goy.37@gmail.com>
2023-06-16 13:55:06 +0200

    create model for this books and change path inner book route because is bad

--- routes/book.js
+++ routes/book.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const bookCtrl = require('../controllers/book');
+const bookCtrl = require("../controllers/book");
 
-router.get('/book', bookCtrl.getAllBooks);
+router.get('/', bookCtrl.getAllBooks);
 
 module.exports = router;
