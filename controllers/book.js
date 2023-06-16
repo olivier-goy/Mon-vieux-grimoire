8f75bba26bdc983bd21ce6a51885c2e9d2085809
olivier-goy <olivier.goy.37@gmail.com>
2023-06-16 20:51:47 +0200

    create function for controller find book just one book

--- controllers/book.js
+++ controllers/book.js
@@ -1,5 +1,12 @@
 const Book = require("../models/Book");
 
+
+exports.getOneBook = (req, res, next) => {
+  Book.findOne({ _id: req.params.id })
+    .then((books) => res.status(200).json(books))
+    .catch((error) => res.status(400).json(error));
+}
+
 exports.getAllBooks = (req, res, next) => {
   Book.find()
     .then((books) => res.status(200).json(books))
