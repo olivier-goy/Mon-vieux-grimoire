55044f2c7f68e429d2e2ba6dd162148704a9cddf
olivier-goy <olivier.goy.37@gmail.com>
2023-06-16 18:38:04 +0200

    add route for login and signup

--- routes/user.js
+++ routes/user.js
@@ -3,3 +3,7 @@ const route = express.Router();
 
 const userCtrl = require("../controllers/user");
 
+route.post('/signup', userCtrl.signup);
+route.post('/login', userCtrl.login);
+
+module.exports = route;
