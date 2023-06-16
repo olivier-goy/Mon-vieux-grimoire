8be79004d1f161cdc6f067f2a324b06f21fa2e39
olivier-goy <olivier.goy.37@gmail.com>
2023-06-16 20:53:28 +0200

    create router for find one book

--- routes/book.js
+++ routes/book.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const bookCtrl = require("../controllers/book");
 
+router.get('/:id', bookCtrl.getOneBook);
 router.get('/', bookCtrl.getAllBooks);
 
 module.exports = router;
