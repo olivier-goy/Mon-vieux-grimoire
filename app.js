c8e4753ed0c48d7015238e2b2ad69451c4014d70
olivier-goy <olivier.goy.37@gmail.com>
2023-06-16 11:47:45 +0200

    add connection with DB mongoDB

--- app.js
+++ app.js
@@ -1,16 +1,32 @@
-const express = require('express');
+const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
 
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-    next();
-  });
+mongoose.connect("mongodb+srv://Olivier:Olivier.@atlascluster.2c8xmmi.mongodb.net/?retryWrites=true&w=majority",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
+  .then(() => console.log("Connexion à MongoDB réussi !"))
+  .catch(() => console.log("Connexion à MongoDB échouée !"));
 
 app.use((req, res, next) => {
-  console.log('Requête reçue !');
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  next();
+});
+
+app.use((req, res, next) => {
+  console.log("Requête reçue !");
   next();
 });
 
@@ -20,12 +36,12 @@ app.use((req, res, next) => {
 });
 
 app.use((req, res, next) => {
-  res.json({ message: 'Votre requête a bien été reçue !' });
+  res.json({ message: "Votre requête a bien été reçue !" });
   next();
 });
 
 app.use((req, res, next) => {
-  console.log('Réponse envoyée avec succès !');
+  console.log("Réponse envoyée avec succès !");
 });
 
 module.exports = app;
