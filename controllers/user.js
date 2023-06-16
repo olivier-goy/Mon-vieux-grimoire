9da3d9a0c0b9b7e298c9942ae13e5f055b0bb50d
olivier-goy <olivier.goy.37@gmail.com>
2023-06-16 17:59:13 +0200

    create function for login user with response userid and token

--- controllers/user.js
+++ controllers/user.js
@@ -18,3 +18,33 @@ exports.signup = (req, res, next) => {
     })
     .catch((error) => res.status(500).json(error));
 };
+
+exports.login = (req, res, next) => {
+  User.findOne({ email: req.body.email })
+    .then((user) => {
+      if (!user) {
+        res
+          .status(401)
+          .json({ message: "Mot de passe ou identifiant incorrect !" });
+      } else {
+        bcrypt
+          .compare(req.body.password, user.password)
+          .then((valid) => {
+            if (!valid) {
+              res
+                .status(401)
+                .json({ message: "Mot de passe ou identifiant !" });
+            } else {
+              res.status(200).json({
+                userId: user._id,
+                token: jwt.sign({ userId: user._id }, "RANDOM_TOKEN_SECRET", {
+                  expiresIn: "24H",
+                }),
+              });
+            }
+          })
+          .catch((error) => res.status(500).json(error));
+      }
+    })
+    .catch((error) => res.status(500).json(error));
+};
