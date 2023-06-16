d2303d9eab3c4f58e8074ec912d137de081a47e4
olivier-goy <olivier.goy.37@gmail.com>
2023-06-16 19:38:24 +0200

    import user router and add path auth

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 
+const userRoute = require("./routes/user");
 const bookRoutes = require("./routes/book");
 
 mongoose.connect("mongodb+srv://Olivier:Olivier.@atlascluster.2c8xmmi.mongodb.net/?retryWrites=true&w=majority",
@@ -13,6 +14,8 @@ mongoose.connect("mongodb+srv://Olivier:Olivier.@atlascluster.2c8xmmi.mongodb.ne
 .catch(() => console.log("Connexion à MongoDB échouée !"));
 
 const app = express();
+
+app.use(express.json());
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
@@ -27,6 +30,7 @@ app.use((req, res, next) => {
   next();
 });
 
+app.use("/api/auth", userRoute);
 app.use("/api/book", bookRoutes);
 
 module.exports = app;
