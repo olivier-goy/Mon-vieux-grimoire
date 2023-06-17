c7a0c332c1892e9623a8aaf440e881dd9208e489
olivier-goy <olivier.goy.37@gmail.com>
2023-06-17 15:47:02 +0200

    add function for return bestBook

--- controllers/book.js
+++ controllers/book.js
@@ -35,6 +35,12 @@ exports.deleteBook = (req, res, next) => {
     .catch((error) => res.status(500).json({ error }));
 };
 
+exports.getBestBooks = (req, res, next) => { 
+  Book.find()
+  .then((books) => res.status(200).json(books))
+  .catch((error) => res.status(400).json({ error }));
+}
+
 exports.getOneBook = (req, res, next) => {
   Book.findOne({ _id: req.params.id })
     .then((books) => res.status(200).json(books))
