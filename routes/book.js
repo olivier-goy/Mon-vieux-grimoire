1e6f4723d1eba65b17a61a84ff4c7d93a65200ed
olivier-goy <olivier.goy.37@gmail.com>
2023-06-17 11:04:04 +0200

    import middleware multer for add route

--- routes/book.js
+++ routes/book.js
@@ -1,6 +1,9 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const multer = require("../middlewares/multer-config");
 const bookCtrl = require("../controllers/book");
+
+
 
 router.get('/:id', bookCtrl.getOneBook);
 router.get('/', bookCtrl.getAllBooks);
