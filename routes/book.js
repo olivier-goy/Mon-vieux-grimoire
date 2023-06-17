d85c530791921ed7daf60b208cc96bced0524b02
olivier-goy <olivier.goy.37@gmail.com>
2023-06-17 14:48:26 +0200

    add router for delete methode inner book router

--- routes/book.js
+++ routes/book.js
@@ -4,7 +4,8 @@ const auth = require("../middlewares/auth");
 const multer = require("../middlewares/multer-config");
 const bookCtrl = require("../controllers/book");
 
-router.post('/', auth, multer, bookCtrl.creatingBook)
+router.post('/', auth, multer, bookCtrl.creatingBook);
+router.delete('/:id', auth, bookCtrl.deleteBook);
 router.get('/:id', bookCtrl.getOneBook);
 router.get('/', bookCtrl.getAllBooks);
 
