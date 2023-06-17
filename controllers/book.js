14a74b12dbfd1a70cf22246ec3b0215d80b7ab34
olivier-goy <olivier.goy.37@gmail.com>
2023-06-17 15:21:47 +0200

    resolve bug functionne deleted inner controller juste bad requeste

--- controllers/book.js
+++ controllers/book.js
@@ -8,39 +8,41 @@ exports.creatingBook = (req, res, next) => {
   const book = new Book({
     ...objectBook,
     userId: req.auth.userId,
-    imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
+    imageUrl: `${req.protocol}://${req.get("host")}/images/${
+      req.file.filename
+    }`,
   });
   book
     .save()
-    .then(() => res.status(200).json({ message: 'livre enregistré !' }))
-    .catch(error => res.status(404).json(error));
-}
+    .then(() => res.status(200).json({ message: "livre enregistré !" }))
+    .catch((error) => res.status(404).json({ error }));
+};
 
-exports.deleteBook = (req, res, next) => { 
-  Book.findOne({ _id: req.params._id })
-    .then(book => {
-      if (book.userId !== req.auth.userId) {
-        res.status(403).json({ message: "Unauthorized request" })
+exports.deleteBook = (req, res, next) => {
+  Book.findOne({ _id: req.params.id })
+    .then((book) => {
+      if (book.userId != req.auth.userId) {
+        res.status(403).json({ message: "Not authorized" });
       } else {
-        const filename = req.imageUrl.split("/images/")[1];
+        const filename = book.imageUrl.split("/images/")[1];
         fs.unlink(`images/${filename}`, () => {
-          Book.deleteOne({ _id: req.params._id })
-            .then(() => res.status(200).json({ message: "Livre supprimé !" }))
-            .catch(error => res.status(404).json(error));
-        })
-       }
+          Book.deleteOne({ _id: req.params.id })
+            .then(() => res.status(200).json({ message: "Objet supprimé" }))
+            .catch((error) => res.status(404).json({ error }));
+        });
+      }
     })
-  .catch(error => res.status(500).json(error));
- }
+    .catch((error) => res.status(500).json({ error }));
+};
 
 exports.getOneBook = (req, res, next) => {
   Book.findOne({ _id: req.params.id })
     .then((books) => res.status(200).json(books))
-    .catch((error) => res.status(400).json(error));
-}
+    .catch((error) => res.status(400).json({ error }));
+};
 
 exports.getAllBooks = (req, res, next) => {
   Book.find()
     .then((books) => res.status(200).json(books))
-    .catch((error) => res.status(400).json(error));
+    .catch((error) => res.status(400).json({ error }));
 };
