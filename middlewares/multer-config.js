b3693d8d8e5b12c24501338d06f9ec1b5e491696
olivier-goy <olivier.goy.37@gmail.com>
2023-06-17 10:56:39 +0200

    add methode for multer format image

--- middlewares/multer-config.js
+++ middlewares/multer-config.js
@@ -4,4 +4,15 @@ const MINE_TYPES = {
     'image/jpg': 'jpg',
     'image/jpeg': 'jpg',
     'image/png': 'png'
-}
+}; 
+
+const storage = multer.diskStorage({
+    destination: (req, file, callback) => {
+      callback(null, 'images');
+    },
+    filename: (req, file, callback) => {
+      const name = file.originalname.split(' ').join('_');
+      const extension = MIME_TYPES[file.mimetype];
+      callback(null, name + Date.now() + '.' + extension);
+    }
+  });
