f45ac879669e79f1fb18e5973777b9c922be1c9d
olivier-goy <olivier.goy.37@gmail.com>
2023-06-17 11:05:44 +0200

    add router for post book

--- routes/book.js
+++ routes/book.js
@@ -3,8 +3,7 @@ const router = express.Router();
 const multer = require("../middlewares/multer-config");
 const bookCtrl = require("../controllers/book");
 
-
-
+router.post('/', multer, bookCtrl.creatingBook)
 router.get('/:id', bookCtrl.getOneBook);
 router.get('/', bookCtrl.getAllBooks);
 
