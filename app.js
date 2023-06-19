1e3a769b2f0dc820c213e743ba097321992abf7f
olivier-goy <olivier.goy.37@gmail.com>
2023-06-19 15:10:11 +0200

    deleted params not to use

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const path = require("path");
 
-const userRoute = require("./routes/user");
+const userRoutes = require("./routes/user");
 const bookRoutes = require("./routes/book");
 
 mongoose.connect("mongodb+srv://Olivier:Olivier.@atlascluster.2c8xmmi.mongodb.net/?retryWrites=true&w=majority",
@@ -31,7 +31,7 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use("/api/auth", userRoute);
+app.use("/api/auth", userRoutes);
 app.use("/api/books", bookRoutes);
 app.use('/images', express.static(path.join(__dirname, 'images')));
 
--- controllers/user.js
+++ controllers/user.js
@@ -3,7 +3,7 @@ const jwt = require("jsonwebtoken");
 
 const User = require("../models/User");
 
-exports.signup = (req, res, next) => {
+exports.signup = (req, res) => {
   bcrypt
     .hash(req.body.password, 10)
     .then((hash) => {
@@ -13,13 +13,13 @@ exports.signup = (req, res, next) => {
       });
       user
         .save()
-        .then(() => res.status(200).json({message: "Utilisateur créer !"}))
-        .catch((error) => res.status(400).json(error));
+        .then(() => res.status(201).json({message: "Utilisateur créer !"}))
+        .catch((error) => res.status(400).json({error}));
     })
     .catch((error) => res.status(500).json(error));
 };
 
-exports.login = (req, res, next) => {
+exports.login = (req, res) => {
   User.findOne({ email: req.body.email })
     .then((user) => {
       if (!user) {
@@ -33,7 +33,7 @@ exports.login = (req, res, next) => {
             if (!valid) {
               res
                 .status(401)
-                .json({ message: "Mot de passe ou/et identifiant !" });
+                .json({ message: "Mot de passe ou/et identifiant incorrect !" });
             } else {
               res.status(200).json({
                 userId: user._id,
